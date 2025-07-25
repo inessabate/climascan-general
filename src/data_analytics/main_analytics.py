92be63449dce7c1b2ba8095d5a7665fb700de9c5
inessabate <inessabate17@gmail.com>
2025-07-25 19:57:45 +0200

    Set up repo

---
+++ src/data_analytics/main_analytics.py
@@ -0,0 +1,20 @@
+"""
+Script para Data Analytics:
+- Consultas sobre Delta Lake con DuckDB
+- Entrenamiento de modelos
+"""
+
+import logging
+from src.utils.logging_setup import setup_logging
+
+setup_logging()
+logger = logging.getLogger('main_analytics')
+
+def run_analytics():
+
+    logger.info(" Iniciando módulo de Data Analytics")
+    # TODO: Implementar lógica real
+    logger.info(" Data Analytics completado correctamente.")
+
+if __name__ == "__main__":
+    run_analytics()
